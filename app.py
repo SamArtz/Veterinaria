aa10793d6880da4cc7a7d8fb0b2ef1843663dd7d
Woody07-crypto <ajcruzlinares05@gmail.com>
2025-07-01 16:42:07 -0600

    kkkkkk

--- app.py
+++ app.py
@@ -132,12 +132,30 @@ class ventana_principal(QMainWindow):
 
     def doctor(self):
 
-        self.label=QLabel("doctor")
+        self.label=QLabel("Bienvenido Doctor",self)
+        self.label.setFont(QFont("Arial", 20))
+        self.label.adjustSize()
+        layout_admin = QHBoxLayout()
+        layout_admin.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        self.setLayout(layout_admin)
+
+
+        self.ver_Paciente=QPushButton("Ver pacientes",self)
+        self.ver_Paciente.move(100,150)
+        self.ver_Paciente.clicked.connect(self.mostrar_pacientes)
         
-        layout_admin=QHBoxLayout()
-
-        layout_admin.addWidget(self.label)
-
+        self.pacientes=QComboBox()
+        self.pacientes.addItems(["Paciente 1", "Paciente 2", "Paciente 3"])
+        self.pacientes.hide()
+        layout = QVBoxLayout()
+        layout.addWidget(self.ver_Paciente)
+        layout.addWidget(self.pacientes)
+        self.setLayout(layout)
+        
+    def mostrar_pacientes(self):
+        self.pacientes.show()
+        
+        
         container=QWidget()
         container.setLayout(layout_admin)
         self.setCentralWidget(container)
